bf81c7d4aab06f2c7db13cf6e29b208386bce620
Ilya Samarin <ilia-samarin@bk.ru>
2023-10-15 18:19:39 +0700

    Добавьте файлы проекта.

---
+++ PRTelegramExample/Commands.cs
@@ -0,0 +1,21 @@
+﻿using PRTelegramBot.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace PRTelegramExample
+{
+    public class Commands
+    {
+        [ReplyMenuHandler(true,"Тест","Hello","World")]
+        public static async Task Example(ITelegramBotClient botClient, Update update)
+        {
+            var message = "Hello world";
+            var sendMessage = await PRTelegramBot.Helpers.Message.Send(botClient,update, message);
+        }
+    }
+}
---
+++ PRTelegramExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using PRTelegramBot.Core;
+
+const string EXIT_COMMAND = "exit";
+
+var telegram = new PRBot(option =>
+{
+    option.Token = "";
+    option.ClearUpdatesOnStart = true;
+    option.WhiteListUsers = new List<long>() { };
+    option.Admins = new List<long>() { };
+    option.BotId = 0;
+});
+
+telegram.OnLogCommon += Telegram_OnLogCommon;
+telegram.OnLogError += Telegram_OnLogError;
+
+await telegram.Start();
+
+void Telegram_OnLogError(Exception ex, long? id)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    string errorMsg = $"{DateTime.Now}:{ex}";
+    Console.WriteLine(errorMsg);
+    Console.ResetColor();
+}
+
+void Telegram_OnLogCommon(string msg, PRBot.TelegramEvents typeEvent, ConsoleColor color)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    string message = $"{DateTime.Now}:{msg}";
+    Console.WriteLine(message);
+    Console.ResetColor();
+}
+
+while (true)
+{
+    var result = Console.ReadLine();
+    if(result.ToLower() == EXIT_COMMAND)
+    {
+        Environment.Exit(0);
+    }
+}
+
+
